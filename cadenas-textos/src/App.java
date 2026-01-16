ed7627520ecab85519882b6f0d24aef73d6cc280
Nazaret <nazaretgutirrezmartn@gmail.com>
2026-01-16 09:30:56 +0100

    Ej de string 16E

--- cadenas-textos/src/App.java
+++ cadenas-textos/src/App.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        String respuesta, txt;
+        String respuesta, txt, palabraSust;
         int contador = 0, posicion = 0;
+        boolean sustituir = false;
         
         System.out.print("Dime un texto: ");
         txt = sc.nextLine();
@@ -13,15 +14,25 @@ public class App {
         respuesta = sc.nextLine();
 
         do {
-            posicion = txt.indexOf(respuesta, posicion + 1);
+            posicion = txt.toLowerCase().indexOf(respuesta.toLowerCase(), posicion + 1);
 
-            if (posicion != -1) {
+            //if (posicion != -1) {
                 contador++;
-            }
-
+            //}
+          
         } while (posicion != -1);
+        
+        System.out.printf("La palabra %s aparece %d veces en el texto.\n", respuesta, contador);
 
-        System.out.printf("La palabra %s aparece %d veces en el texto." , respuesta, contador);
+        System.out.print("¿Quieres sustituir la palabra? (s/n): ");
+
+        if (sc.nextLine().equals("s")) {
+            System.out.print("¿Por qué palabra quieres sustituir?: ");
+            palabraSust = sc.nextLine();
+
+            String nuevoTxt = txt.toLowerCase().replace(respuesta.toLowerCase(), palabraSust);
+            System.out.println("El nuevo texto es: \n" + nuevoTxt);
+        }
 
         sc.close();
     }
