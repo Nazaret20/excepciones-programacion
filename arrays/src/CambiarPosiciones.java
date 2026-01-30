4615d369ec2de5d61bb0070685b61d64cb5d5082
Nazaret <nazaretgutirrezmartn@gmail.com>
2026-01-30 09:32:20 +0100

    ejercicios 30E

--- arrays/src/CambiarPosiciones.java
+++ arrays/src/CambiarPosiciones.java
@@ -1,11 +1,44 @@
+import java.util.Arrays;
+
 public class CambiarPosiciones {
     public static void main(String[] args) {
-        int[] nums = {10, 12, 13, 14};
-        int[] posiciones = {2, 1 ,3, 0};
+        int[] nums = {10, 11, 12, 13};
+        int[] posiciones = {3, 0, 2, 1};
+
+        int[] fnCambiar = cambiarPos(nums, posiciones);
+
+        if (fnCambiar != null) {
+            System.out.printf("El array %s con las nuevas posiciones de %s, será %s.", Arrays.toString(nums), Arrays.toString(posiciones), Arrays.toString(fnCambiar));
+        } else {
+            System.err.println("Había un error de entrada en los arrays.");
+        }
+        
     }
 
     public static int[] cambiarPos(int[] nums, int[] posiciones) {
+        if (nums.length != posiciones.length) {
+            return null;
+        }
+
         int[] result = new int[nums.length];
+
+        for (int i = 0; i < posiciones.length; i++) {
+            if (posiciones[i] >= posiciones.length) {
+                return null;
+            }
+
+            for (int j = i + 1; j < posiciones.length - 1; j++) {
+                if (posiciones[i] == posiciones[j]) {
+                    return null;
+                }
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            result[posiciones[i]] = nums[i];
+        }
+        
+
         return result;
     }
 }
---
+++ arrays/src/Interseccion.java
@@ -0,0 +1,7 @@
+public class Interseccion {
+
+    public static void main(String[] args) {
+        int[] array1 = {1, 9, 3, 5, 2};
+        int[] array2 = {3, 5, 4, 8, 3};
+    }
+}
